46ea629d4f524fa8cfb59b1d1af5ac03ab80f0c3
SofyanZa <soza@live.fr>
2022-07-24 22:29:21 +0200

    add weatherview

---
+++ BaluchonProjectZ/View/WeatherView.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherView.swift
+//  BaluchonProject
+//
+//  Created by Sofyan Zarouri on 18/07/2022
+//  Copyright © 2022 Sofyan Zarouri. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+    // images meteos avec leur clé
+    static let icon: [String: UIImage] = ["Mist": #imageLiteral(resourceName: "Mist")  , "Haze": #imageLiteral(resourceName: "Fog") , "Clouds":#imageLiteral(resourceName: "Clouds") , "Lighting":#imageLiteral(resourceName: "Lightning") , "Rain":#imageLiteral(resourceName: "Rain") , "Snow": #imageLiteral(resourceName: "Snow"), "Clear":#imageLiteral(resourceName: "Clear"),"Drizzle":#imageLiteral(resourceName: "Drizzle_"),"Fog":#imageLiteral(resourceName: "Haze") ]
+}
+
+
